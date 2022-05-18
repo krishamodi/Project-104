fac1739d61a4fe7907c136ce2f2fe25c80fa74cd
krishamodi <95953266+krishamodi@users.noreply.github.com>
2022-05-18 14:21:57 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:'png',
+    png_quality:100
+});
+
+
+Webcam.attach('#camera');
+
+    function take_snapshot()
+    {
+        Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'">';
+        });
+    }
+
+
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/hxNx3n94j/model.json',modelLoaded);
+
+
+function modelLoaded(){
+    console.log('Model Loaded!');
+}
